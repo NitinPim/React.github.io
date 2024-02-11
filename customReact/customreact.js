306ae8c4ba0d7fd55cbe6b95a548ad822159d945
Nitin Pimpalkar <nitinpimpalkar17@gmail.com>
2024-02-11 21:08:19 +0530

    add customReact

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,2 +1,22 @@
- const element = document.querySelector('#root')
- element.render()
+function customRender(reactElement, container){
+    const domElementContainer = document.createElement(reactElement.type)
+    domElementContainer.innerHTML = reactElement.children
+    domElementContainer.setAttribute('href', reactElement.props.href)
+    domElementContainer.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElementContainer)
+}
+
+const reactElement ={
+    type:'a',
+    props: {
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children:'click me to visit google'
+ }
+ 
+ const mainContainer = document.querySelector('#root')
+
+ customRender(reactElement, mainContainer)
+ 
